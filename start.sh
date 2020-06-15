a4ff010fb0d16bb2a33661c0ce07be83664fcfae
ivxxi <crystalalice21@gmail.com>
2020-06-15 11:35:12 +0300

    first commit

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='qwertys'
+export DATABASE_URL='postgres://fuffrcocybnszz:8d493f9916b524ba1c8c6c0922f26b8ae45eb1ffd87a0775eeca451f2181c165@ec2-54-235-220-220.compute-1.amazonaws.com:5432/d804lilg5dv5kg'
+#export DATABASE_URL='postgres://nuesqzseeskvis:b8f5358699a9bef7ee96ec3378016fd50e216c079514fc80b9c2543402029f8e@ec2-54-197-48-79.compute-1.amazonaws.com:5432/d7c2nfng23mcdt'
+#export DATABASE_URL="sqlite:///db.sqlite3"
+
+python3.6 manage.py server
